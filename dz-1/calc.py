dca354f79954438c7465e2e92b5adb84538dde97
Roman Deles <bullsey@gmail.com>
2015-10-12 22:59:46 +0300

    first commit

---
+++ dz-1/calc.py
@@ -0,0 +1,26 @@
+__author__ = 'romandeles'
+
+math_ops = ['+', '-', '*', '/', '%']
+
+users_input = input("type number #1: ")
+users_input = users_input.replace(",",".")
+for i in users_input:
+    if i in math_ops:
+        num_index = users_input.index(i)
+number1 = users_input[:num_index]
+sign = users_input[num_index:num_index + 1]
+number2 = users_input[num_index + 1:]
+
+if sign is "+":
+    result = float(number1) + float(number2)
+if sign is "-":
+    result = float(number1) - float(number2)
+if sign is "*":
+    result = float(number1) * float(number2)
+if sign is '/':
+    result = float(number1) / float(number2)
+if sign is '%':
+    result = float(number1) % float(number2)
+
+
+print(result)
