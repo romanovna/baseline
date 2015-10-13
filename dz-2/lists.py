a5917bd3fc07b66c6e623405809f985000d15284
unknown <roman.deles@ROMANDELES.int.nordigy.ru>
2015-10-13 12:35:40 +0300

    2nd step

--- dz-2/lists.py
+++ dz-2/lists.py
@@ -1,5 +1,5 @@
 __author__ = 'roman.deles'
-#coding: utf-8
+# coding: utf-8
 """
 1) Создаем список, заполняем (имя + фамилия)
 
@@ -20,10 +20,14 @@ list_of_students = []
 
 while len(list_of_students) < 5:
     name = input('type name: ')
-    if name.isdigit() is False:
+    if name.isalpha() is True:
         list_of_students.append(name)
         print('Ok, sounds good')
     else:
         print('You should type only letters')
 
 print(list_of_students)
+
+single_choice = input('Type index of the {0}: '.format(list_of_students))
+
+print('You have selected {0}'.format(list_of_students[int(single_choice)]))
