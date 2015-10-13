a88905fa41cddc2183ac02b155bfdff591e6b367
unknown <roman.deles@ROMANDELES.int.nordigy.ru>
2015-10-13 08:46:43 +0300

    + description

--- dz-1/calc.py
+++ dz-1/calc.py
@@ -1,9 +1,14 @@
 __author__ = 'romandeles'
+#coding: utf-8
+
+"""Дефолтный калькулятор
+замена запятой на точку - присутствует
+"""
 
 math_ops = ['+', '-', '*', '/', '%']
 
 users_input = input("type number #1: ")
-users_input = users_input.replace(",",".")
+users_input = users_input.replace(",", ".")
 for i in users_input:
     if i in math_ops:
         num_index = users_input.index(i)
@@ -21,6 +26,5 @@ if sign is '/':
     result = float(number1) / float(number2)
 if sign is '%':
     result = float(number1) % float(number2)
-
 
 print(result)
