143dd430f1e986a5020a4e20614005b8715ac509
unknown <roman.deles@ROMANDELES.int.nordigy.ru>
2015-10-15 17:37:14 +0300

    commit

--- dz-3/smthng.py
+++ dz-3/smthng.py
@@ -10,3 +10,27 @@ output : выводятся все алфавиту со стандартной 
 
 по мощности
 по названию
+"""
+
+import pickle
+
+commands = ['input','output']
+listen_command = ''
+auto_key ={}
+
+while True:
+    listen_command = input('Type the command of the list {0}'.format(commands))
+    if listen_command in commands:
+        break
+    else:
+        print('Please type {0} or {1}'.format(commands[0],commands[1]))
+
+input_data = ''
+
+if listen_command == commands[0]:
+    print('Ok, you have chosen {0} some data'.format(commands[0]))
+    input_data = input('Type something ').split()
+    print(input_data)
+    auto_key = dict(input_data)
+
+print(auto_key)
