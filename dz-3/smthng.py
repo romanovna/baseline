e383b2753d13731e23fe511cc2553b0942fb5f4d
Roman Deles <bullsey@gmail.com>
2015-10-16 20:28:06 +0300

    dz3

--- dz-3/smthng.py
+++ dz-3/smthng.py
@@ -12,20 +12,55 @@ output : выводятся все алфавиту со стандартной 
 по названию
 """
 
-import pickle,sys
+import pickle, sys
 
-commands = ['input','output','exit']
-listen_command = ''
-auto_key ={}
 
+def load_commands():
+    commands = ['input', 'output', 'exit']
+    return commands
+
+
+def input_command():
+    input1 = input('Type the command of the list {0}: '.format(load_commands()))
+    if input1 == load_commands()[0]:
+        print('Ok, you have chosen to {0} some data'.format(load_commands()[0]))
+        _input_data()
+    elif input1 == load_commands()[1]:
+        print('Ok, you have chosen to {0} some data'.format(load_commands()[1]))
+        load_data()
+    elif input1 == load_commands()[2]:
+        print('Ok, you have chosen to {0} from the program'.format(load_commands()[2]))
+        sys.exit()
+    else:
+        print('U have to choose one of the command from the list')
+        input_command()
+
+
+def _input_data():
+    input_data = input('Type CAR and HP ')
+    auto_key = dict(input_data.split() for s in input_data)
+    for key, value in auto_key.items():
+        if key.isalpha() and value.isdigit():
+            with open('CARS.db', 'wb') as f:
+                pickle.dump(auto_key, f)
+        else:
+            print('Please type correct items')
+            _input_data()
+    repeat = input('do you wanna add some more? Y or N ')
+    if repeat == 'Y':
+        _input_data()
+    else:
+        input_command()
+
+
+def load_data():
+    with open('CARS.db', 'rb') as f:
+        print(pickle.load(f))
+
+
+"""
 while True:
     listen_command = input('Type the command of the list {0}: '.format(commands))
-#    if listen_command in commands:
-#        break
-#    else:
-#        print('Please type {0} or {1}'.format(commands[0],commands[1]))
-#        input_data = ''
-
     if listen_command == commands[0]:
         print('Ok, you have chosen to {0} some data: '.format(commands[0]))
         input_data = input('Type CAR and HP ')
@@ -43,3 +78,6 @@ while True:
         print(cars)
     if listen_command == commands[2]:
         sys.exit()
+"""
+
+input_command()
