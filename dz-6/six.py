ca1dc1e788f05e089f2e8ae25bb199c1ac7f73c1
Roman Deles <bullsey@gmail.com>
2015-11-02 15:13:21 +0300

    upd

--- dz-6/six.py
+++ dz-6/six.py
@@ -5,13 +5,13 @@ def find(ext='.py'):
     for filename in os.listdir('.'):
         if filename.endswith(ext):
             for i,line in enumerate(open(filename),1):
-                if 'def' in line:
                     yield filename,i,line
 
 
 def grep(gen,substr):
     for name,i,s in gen:
-        yield name,i,s
+        if substr in s:
+            yield name,i,s
 
 
 
