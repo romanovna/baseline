b4584982095189a0706ecfb1160d06120f93eb4d
Roman Deles <bullsey@gmail.com>
2015-11-18 16:56:53 +0300

    xz

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -10,7 +10,10 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return self.pub_date >= timezone.now() - datetime.timedelta(hours=1)
+    was_published_recently.admin_order_field = 'pubdate'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
     def choices_given(self):
         return self.choice_set.count()
