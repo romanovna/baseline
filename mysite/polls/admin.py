e75256e4ed0564416b659310a1266966b488621f
Roman Deles <bullsey@gmail.com>
2015-11-18 16:48:20 +0300

    xz

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Question,Choice
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -14,6 +14,8 @@ class QuestionAdmin(admin.ModelAdmin):
     (None, {'fields': ['question_text']}),
     ('Date Info',{'fields': ['pub_date'],'classes':['collapse']})]
     inlines = [ChoiceInline]
+    list_display = ('question_text','pub_date','was_published_recently','choices_given')
+
 
 
 
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -12,6 +12,9 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+    def choices_given(self):
+        return self.choice_set.count()
+
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
