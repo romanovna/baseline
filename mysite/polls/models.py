fc85bcd5040c37fd0054568d0413584863f544f9
romanovna <bullsey@gmail.com>
2015-11-25 22:08:21 +0300

    add test

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -10,7 +10,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(hours=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     was_published_recently.admin_order_field = 'pubdate'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
@@ -25,3 +26,4 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
