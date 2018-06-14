d2d5300d0763db541031ec65ac9d1967361e70c0
Roman Deles <bullsey@gmail.com>
2018-06-14 16:52:46 +0300

    + roman numerals

---
+++ pycheckio_tests/electronic_station/reverse_roman_numerals.py
@@ -0,0 +1,19 @@
+def reverse_roman(s):
+    rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    int_val = 0
+    for i in range(len(s)):
+        if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
+            int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
+        else:
+            int_val += rom_val[s[i]]
+    return int_val
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert reverse_roman('I') == 1, '1'
+    assert reverse_roman('VI') == 6, '6'
+    assert reverse_roman('LXXVI') == 76, '76'
+    assert reverse_roman('CDXCIX') == 499, '499'
+    assert reverse_roman('MMMDCCCLXXXVIII') == 3888, '3888'
+    print('Great! It is time to Check your code!')
