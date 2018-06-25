bb1433612fa0026b8f6080763bce42413488a0f1
Roman Deles <bullsey@gmail.com>
2018-06-25 17:15:26 +0300

    + worth of words

---
+++ pycheckio_tests/oreilly/worth_of_words.py
@@ -0,0 +1,24 @@
+VALUES = {'e': 1, 'a': 1, 'i': 1, 'o': 1, 'n': 1, 'r': 1,
+          't': 1, 'l': 1, 's': 1, 'u': 1, 'd': 2, 'g': 2,
+          'b': 3, 'c': 3, 'm': 3, 'p': 3, 'f': 4, 'h': 4,
+          'v': 4, 'w': 4, 'y': 4, 'k': 5, 'j': 8, 'x': 8,
+          'q': 10, 'z': 10}
+
+
+def worth_of_words(words):
+    res = {k: 0 for k in words}
+
+    for word in words:
+        for ch in word:
+            res[word] += VALUES[ch]
+    return max(res, key=res.get)
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(worth_of_words(['hi', 'quiz', 'bomb', 'president']))
+
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert worth_of_words(['hi', 'quiz', 'bomb', 'president']) == 'quiz'
+    assert worth_of_words(['zero', 'one', 'two', 'three', 'four', 'five']) == 'zero'
+    print("Coding complete? Click 'Check' to earn cool rewards!")
