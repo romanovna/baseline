dd9b9652ac2e2fc035351941d1d7b4d419f7a9fb
Roman Deles <bullsey@gmail.com>
2018-07-02 13:38:17 +0300

    + flatten-dict

---
+++ pycheckio_tests/github/flatten-dict.py
@@ -0,0 +1,42 @@
+def flatten(d):
+    def items():
+        for key, value in d.items():
+            if isinstance(value, dict):
+                if value:
+                    for subkey, subvalue in flatten(value).items():
+                        yield key + "/" + subkey, subvalue
+                else:
+                    yield key, ""
+            else:
+                yield key, value
+
+    return dict(items())
+
+
+if __name__ == '__main__':
+    test_input = {"key": {"deeper": {"more": {"enough": "value"}}}}
+    print(' Input: {}'.format(test_input))
+    print('Output: {}'.format(flatten(test_input)))
+
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert flatten({"key": "value"}) == {"key": "value"}, "Simple"
+    assert flatten(
+        {"key": {"deeper": {"more": {"enough": "value"}}}}
+    ) == {"key/deeper/more/enough": "value"}, "Nested"
+    assert flatten({"empty": {}}) == {"empty": ""}, "Empty value"
+    assert flatten({"name": {
+        "first": "One",
+        "last": "Drone"},
+        "job": "scout",
+        "recent": {},
+        "additional": {
+            "place": {
+                "zone": "1",
+                "cell": "2"}}}
+    ) == {"name/first": "One",
+          "name/last": "Drone",
+          "job": "scout",
+          "recent": "",
+          "additional/place/zone": "1",
+          "additional/place/cell": "2"}
+    print('You all set. Click "Check" now!')
