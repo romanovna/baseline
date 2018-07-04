a99e2f275fdf351aab2169807350226d8159388a
Roman Deles <bullsey@gmail.com>
2018-07-04 16:17:23 +0300

    + long repeat

---
+++ pycheckio_tests/Incinerator/long-repeat-inside.py
@@ -0,0 +1,16 @@
+import re
+
+
+def repeat_inside(line):
+    res = [i[0] + i[1] for i in re.findall(r'(?=(.+?)(\1+))', line)]
+    return max(a, key=len) if res else ''
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert repeat_inside('aaaaa') == 'aaaaa', "First"
+    assert repeat_inside('aabbff') == 'aa', "Second"
+    assert repeat_inside('aababcc') == 'abab', "Third"
+    assert repeat_inside('abc') == '', "Forth"
+    assert repeat_inside('abcabcabab') == 'abcabc', "Fifth"
+    print('"Run" is good. How is "Check"?')
